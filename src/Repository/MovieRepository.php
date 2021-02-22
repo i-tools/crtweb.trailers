af16b2dc0dd35adbe89912c140c525bfe5ee333c
Alex Krasnov <alex@i-tools.ru>
2021-02-23 00:36:04 +0500

    Added method for reading trailer by id

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -3,7 +3,18 @@
 namespace App\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class MovieRepository extends EntityRepository
 {
+    public function findById(int $id, int $mode = Query::HYDRATE_ARRAY): ?array
+    {
+        $result = $this->createQueryBuilder('m')
+            ->andWhere('m.id = '.$id)
+            ->getQuery()
+            ->getResult($mode)
+            ;
+
+        return ($result) ? $result[0] : null;
+    }
 }
