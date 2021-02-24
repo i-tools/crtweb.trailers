630c96fb85b13224db95d4595bb036a2864416d5
Alex Krasnov <alex@i-tools.ru>
2021-02-24 12:28:22 +0500

    Fixed return result

--- src/Repository/LikesRepository.php
+++ src/Repository/LikesRepository.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\Likes;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
-class LikeRepository extends EntityRepository
+class LikesRepository extends EntityRepository
 {
+    public function findByMovieId(int $id, int $mode = Query::HYDRATE_ARRAY): ?array
+    {
+        $result = $this->createQueryBuilder('l')
+            //->join('m.likes', 'l')
+            ->andWhere('l.movie = '.$id)
+            ->getQuery()
+            ->getResult($mode)
+        ;
 
+        return ($result) ? $result[0] : null;
+    }
+
+    public function getByMovieId(int $id): ?Likes
+    {
+        /** @var Likes $like */
+        $like = $this->createQueryBuilder('l')
+            ->andWhere('l.movie = '.$id)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+
+        return $like;
+    }
 }
